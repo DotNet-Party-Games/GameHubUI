fd910121da31479809c9ef6132719707ac641ac9
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-08-31 14:00:32 -0400

    Added button for user to view their custom categories and words

--- projects/songs-ink/src/app/components/lobby/lobby.component.ts
+++ projects/songs-ink/src/app/components/lobby/lobby.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SocketIoService } from './../../services/socketio.service'
 import { RoomListComponent } from '../room-list/room-list.component';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
@@ -9,10 +10,15 @@ import { RoomListComponent } from '../room-list/room-list.component';
 })
 export class LobbyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
+  }
+
+  viewProfile()
+  {
+    this.router.navigate(['profile'], {relativeTo: this.route.parent})
   }
 
 }
