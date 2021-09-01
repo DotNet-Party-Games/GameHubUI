a3ed0b0a50f1d814bf893ad7d0467ff9c22c316a
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-09-01 10:22:25 -0400

    Updated method for retrieving user categories

--- projects/songs-ink/src/app/services/choose-category.service.ts
+++ projects/songs-ink/src/app/services/choose-category.service.ts
@@ -6,14 +6,15 @@ import { Observable } from 'rxjs';
 })
 export class ChooseCategoryService {
 
-  private url = "https://localhost:5001/api/Main";
+  private url = "http://localhost:5002/api/Main";
+ //private url = "https://localhost:5001/api/Main";
   constructor(private http: HttpClient) { }
 
   getDefaultCategories() {
     return this.http.get<any>(`${this.url}/getAllCategories`);
   }
 
-  getUserAndDefaultCategories(userID: number) {
-    return this.http.get<string[]>(this.url);
+  getUserCategories(userID: number) {
+    return this.http.get<string[]>(`${this.url}/${userID}`);
   }
 }
