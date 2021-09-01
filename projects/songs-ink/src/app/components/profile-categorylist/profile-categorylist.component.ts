7e3899a264ce287b821c1971fa504f106b20cb80
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-09-01 10:23:29 -0400

    Updated to use the profile service for retrieving custom words

--- projects/songs-ink/src/app/components/profile-categorylist/profile-categorylist.component.ts
+++ projects/songs-ink/src/app/components/profile-categorylist/profile-categorylist.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-
+import { CustomCategory } from '../../models/CustomCategory';
+import { CustomWord } from '../../models/CustomWord';
+import { ProfileService } from '../../services/profile.service';
 @Component({
   selector: 'app-profile-categorylist',
   templateUrl: './profile-categorylist.component.html',
@@ -8,10 +10,25 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class ProfileCategorylistComponent implements OnInit {
 
   @Output() onRemoveCategory: EventEmitter<string> = new EventEmitter();
-  @Input() category: string;
-  constructor() { }
+  @Input() category: CustomCategory;
+  words: CustomWord[];
+  newWord: string;
+  test: string;
+  Word: CustomWord;
+  wordAddCost: number = -100;
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.Word = 
+    {
+      CustomCategoryId: 0,
+      customWordName: "",
+      PlayerId: this.category.playerId
+    }
+    this.words = [];
+    this.profileService.getCustomWords(this.category.id).subscribe( (response) => {
+      this.words = response;
+    })   
   }
 
   removeCategory(category: string)
@@ -19,4 +36,39 @@ export class ProfileCategorylistComponent implements OnInit {
     this.onRemoveCategory.emit(category);
   }
 
+  addWord(categoryId: number)
+  {
+    this.Word.CustomCategoryId = categoryId;
+    this.Word.customWordName = this.newWord;
+    this.Word.PlayerId = this.category.playerId;
+    //might need to add player id
+    this.profileService.addPlayerWord(this.Word).subscribe( (response) => {
+      this.words.push(
+        {
+          id: response.id,
+          PlayerId: this.Word.PlayerId,
+          customWordName: this.Word.customWordName,
+          CustomCategoryId: this.Word.CustomCategoryId
+        }
+      )
+    }); 
+  }
+
+  removeWord(wordName: string)
+  {
+    let removeWord = this.words.find(word => word.customWordName == wordName);
+
+    if(removeWord)
+    {
+      this.profileService.removePlayerWord(removeWord).subscribe( (result) => {
+        const index = this.words.indexOf(removeWord, 0);
+        this.words.splice(index, 1);
+      })
+    }
+    else 
+    {
+      alert("That word does not exist");
+      return;
+    }
+  }
 }
