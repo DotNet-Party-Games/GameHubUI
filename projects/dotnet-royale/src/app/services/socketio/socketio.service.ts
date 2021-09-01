2eee351676cda6206ca22cc61488748c583e46f0
syoung908 <syoung908@gmail.com>
2021-09-01 07:56:53 -0700

    bugfixes

--- projects/dotnet-royale/src/app/services/socketio/socketio.service.ts
+++ projects/dotnet-royale/src/app/services/socketio/socketio.service.ts
@@ -12,7 +12,7 @@ export class SocketioService {
 
   //private url = 'http://localhost:3000';
   //private url = 'wss://revabox.eastus.cloudapp.azure.com/dotnetroyalesocket/';
-  private url = 'http://revabox.eastus.cloudapp.azure.com';
+  private url = 'https://revabox.eastus.cloudapp.azure.com';
 
   private socket: Socket;
 
